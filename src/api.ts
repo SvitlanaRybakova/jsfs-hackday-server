95a7f4e4bf8a855ec6fe69b907044824272bf6ef
Svitlana <rsv0011@gmail.com>
2023-12-08 09:24:51 +0100

    fix: group photos by collection title

--- src/api.ts
+++ src/api.ts
@@ -20,11 +20,20 @@ router.get("/collections", async (req, res) => {
       .get();
 
     const collections = querySnapshot.docs.map((doc) => ({
-      id: doc.id,
       ...doc.data(),
     }));
 
-    res.status(200).json({ collections });
+
+   const groupedCollections = collections.reduce((acc, photo) => {
+    const { collection } = photo;
+    if (!acc[collection]) {
+      acc[collection] = [];
+    }
+    acc[collection].push(photo);
+    return acc;
+  }, {});
+         
+    res.status(200).json({ groupedCollections });
   } catch (error) {
     console.error("Error fetching photo collections:", error);
     res.status(500).json({ error: "Failed to fetch photo collections" });
