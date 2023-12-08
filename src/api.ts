9904cd6bb714322938fa1e3d3472a3cb9d77e032
Svitlana <rsv0011@gmail.com>
2023-12-08 09:13:41 +0100

    feat: add GET route to retrieve all photo collections

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,34 @@
 import express from "express";
+import { db } from "../src/index";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello Word!");
+type Photo = {
+  photoId: string;
+  collection: string;
+  created: Date;
+  path: string;
+  url: string;
+  like: boolean;
+};
+
+router.get("/collections", async (req, res) => {
+  try {
+    const querySnapshot = await db
+      .collection("photos")
+      .orderBy("created", "desc")
+      .get();
+
+    const collections = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    res.status(200).json({ collections });
+  } catch (error) {
+    console.error("Error fetching photo collections:", error);
+    res.status(500).json({ error: "Failed to fetch photo collections" });
+  }
 });
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import express, { Express } from "express";
-import cors from "cors";
+import { config } from "dotenv";
 import apiRouter from "./api";
+import cors from "cors";
+import admin from "firebase-admin";
+import * as serviceAccount from "../serviceAccountKey.json";
 
+config();
 
+const firebaseConfig = {
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  storageBacket: process.env.BACKET_URL
+};
+
+admin.initializeApp(firebaseConfig);
+
+export const db = admin.firestore();
+export const storage = admin.storage();
+export const bucket = admin.storage().bucket(process.env.BACKET_URL);
 
 const app: Express = express();
 const port = 3001;
